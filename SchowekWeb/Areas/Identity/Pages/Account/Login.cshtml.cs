78414979f76ddc2389d28f8d8c94d01e6d22d338
Marcin Chmurowski <sisimoski@outlook.com>
2021-01-07 21:33:31 +0100

    MainLayout changed

--- SchowekWeb/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ SchowekWeb/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -51,7 +51,7 @@ namespace SchowekWeb.Areas.Identity.Pages.Account
             [DataType(DataType.Password)]
             public string Password { get; set; }
 
-            [Display(Name = "Remember me?")]
+            [Display(Name = "Zapamiętaj mnie?")]
             public bool RememberMe { get; set; }
         }
 
